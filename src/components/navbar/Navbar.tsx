4b88357a3b7c2d5d2017ee3f73979e6e8c9a9d40
Kuba <kuba.krol.ldz@gmail.com>
2024-12-25 22:01:50 +0100

    dodanie komponent dropdown jezyków

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -2,21 +2,29 @@
 
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
-import Sun from "../icons/Sun";
-import Moon from "../icons/Moon";
 import LogoDark from "../icons/LogoDark";
 import { useTheme } from "next-themes";
 import Logo from "../icons/Logo";
 import Github from "../icons/Github";
 import Discord from "../icons/Discord";
+import { useState } from "react";
+import { Languages } from "../icons/Flags";
+import { Check, Moon, ChevronsUpDown } from "lucide-react";
+import { Sun } from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
 
 export default function Navbar() {
     const { theme, setTheme } = useTheme();
-
     const isDarkMode = theme === "dark";
     const Icon = isDarkMode ? Sun : Moon;
     const NavbarLogo = isDarkMode ? <Logo /> : <LogoDark />;
-
 
     return (
         <header className="sticky top-0 container mx-auto py-4 px-3 border-l border-b border-r border-[#000f]/15 dark:border-white/15">
@@ -29,7 +37,7 @@ export default function Navbar() {
                     </h2>
                 </Link>
                 <ul></ul>
-                <div className="flex">
+                <div className="flex items-center">
                     <Button asChild variant="ghost" size="icon" className={`bg-transparent ${isDarkMode ? '' : 'hover:bg-[#000f]/5'}`}>
                         <Link href={"https://discord.gg/f85Eybah"}><Discord /></Link>
                     </Button>
@@ -39,6 +47,14 @@ export default function Navbar() {
                     <Button variant="ghost" size="icon" className={`bg-transparent ${isDarkMode ? '' : 'hover:bg-[#000f]/5'}`} onClick={() => setTheme(isDarkMode ? 'light' : 'dark')}>
                         <Icon />
                     </Button>
+                    <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                            <Button variant="ghost" className={`bg-transparent flex items-center justify-center ${isDarkMode ? '' : 'hover:bg-[#000f]/5'}`} >
+                                {Languages[1].icon}
+                                <ChevronsUpDown />
+                            </Button>
+                        </DropdownMenuTrigger>
+                    </DropdownMenu>
                 </div>
             </nav>
         </header>
